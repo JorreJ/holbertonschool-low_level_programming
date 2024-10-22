316a5f24b650b7be693654d9d9e2f894c19dc0dd
JorreJ <jade.jorre@gmail.com>
2024-10-22 12:45:04 +0000

    modify print_to_98.c programm

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,22 +9,32 @@
 void print_to_98(int n)
 {
 int i, s;
-if (n < 98)
+if (n <= 98)
 {
-for (i = n; i < 98; i++)
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
 {
 printf("%d, ", i);
 }
-}
-else if (n > 98)
+else if (i == 98)
 {
-for (s = n; s > 98; s--)
+printf("%d\n", i);
+}
+}
+}
+else if (n >= 98)
+{
+for (s = n; s >= 98; s--)
+{
+if (s != 98)
 {
 printf("%d, ", s);
 }
-}
-else
+else if (s == 98)
 {
-printf("%d\n", n);
+printf("%d\n", s);
+}
+}
 }
 }
