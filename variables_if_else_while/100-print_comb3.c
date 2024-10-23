c276bd77f8747f6f98e2966cb7015c8649db11e7
JorreJ <jade.jorre@gmail.com>
2024-10-23 11:12:32 +0000

    modify print_comb3.c programm

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
  *
- * main - Entry point
- *
- * Return: Always 0
- *
- **/
+ * Return: Always 0.
+ */
 
 int main(void)
 {
