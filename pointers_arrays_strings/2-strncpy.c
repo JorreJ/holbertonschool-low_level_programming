8cbd308baa3fbb1b549fa8320ef340b232ea2c0e
JorreJ <jade.jorre@gmail.com>
2024-10-30 15:19:58 +0000

    create strncpy.c programm

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the resulting string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+  int l;
+  for (l = 0; l < n && src[l] != '\0'; l++)
+    {
+      dest[l] = src[l];
+    }
+  while (l < n)
+    {
+      dest[l] = '\0';
+      l++;
+    }
+  return (dest);
+}
