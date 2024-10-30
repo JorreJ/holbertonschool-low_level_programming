01fe995bfd0c31cc38777f87d36175092e7d5bf4
JorreJ <jade.jorre@gmail.com>
2024-10-30 10:44:08 +0000

    create print_array.c programm

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
+ * @a: array to be printed
+ * @n: number of elements to print
+ */
+
+void print_array(int *a, int n)
+{
+  int l;
+  for (l = 0; l < n; l++)
+    {
+      printf("%d", a[l]);
+      if (l < n - 1)
+	{
+	  putchar(',');
+	  putchar(' ');
+	}
+    }
+  putchar('\n');
+}
