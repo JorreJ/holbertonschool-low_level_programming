eef33937368a71c917667ddebcb4c1754fbd1a1f
JorreJ <jade.jorre@gmail.com>
2024-10-31 20:01:54 +0000

    modify atoi.c programm

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,37 +11,31 @@
 
 int _atoi(char *s)
 {
-int l;
-int r = 0;
-int n = 0;
-int ns = 0;
-for (l = 0; l < _strlen(s); l++)
-{
-if (s[l] == '-')
-{
-ns++;
-if (ns % 2 == 0)
-{
-ns = 0;
-}
-}
-if (s[l] >= 48 && s[l] <= 57)
-{
-printf("testif: %d, %d, %d ", n, l, ns);
-while (s[l] >= 48 && s[l] <= 57)
-{
-n = ((n * 10) + s[l] - 48);
-l++;
-printf("testwhile: %d, %d, %d ", n, l, ns);
-}
-break;
-}
-}
-if (ns % 2 != 0)
-{
-r = n;
-n = n - r;
-n = n - r;
-}
-return (n);
+  int l;
+  int n = 0;
+  int ns = 0;
+  for (l = 0; l < _strlen(s); l++)
+    {
+      if (s[l] == '-')
+	{
+	  ns++;
+	  if (ns % 2 == 0)
+	    {
+	      ns = 0;
+	    }
+	}
+      if (s[l] >= '0' && s[l] <= '9')
+	{
+	  while (s[l] >= '0' && s[l] <= '9')
+	    {
+	      n = ((n * 10) + (s[l] - '0'));
+	      l++;
+	    }
+	}
+    }
+  if (ns % 2 != 0)
+    {
+      n = -n;
+    }
+  return (n);
 }
