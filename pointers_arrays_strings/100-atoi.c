8269909820ac1ab15983a0ca6e2efa880439f008
JorreJ <jade.jorre@gmail.com>
2024-11-01 10:50:49 +0000

    modify atoi.c programm

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,41 +10,41 @@
 
 int _atoi(char *s)
 {
-  int l;
-  int n = 0;
-  int ns = 0;
-  for (l = 0; l < _strlen(s); l++)
-    {
-      if (s[l] == '-')
-	{
-	  ns++;
-	  if (ns % 2 == 0)
-	    {
-	      ns = 0;
-	    }
-	}
-      if (s[l] >= '0' && s[l] <= '9')
-	{
-	  while (s[l] >= '0' && s[l] <= '9')
-	    {
-	      n = n * 10;
-	      if (n == 2147483640)
-		{
-		  n = n - 1;
-		}
-	      n = n + (s[l] - '0');
-	      l++;
-	    }
-	  break;
-	}
-    }
-  if (ns % 2 != 0)
-    {
-      n = -n;
-      if (n == -2147483647)
-	{
-	  return (n - 1);
-	}
-    }
-  return (n);
+int l;
+int n = 0;
+int ns = 0;
+for (l = 0; l < _strlen(s); l++)
+{
+if (s[l] == '-')
+{
+ns++;
+}
+if (s[l] >= '0' && s[l] <= '9')
+{
+while (s[l] >= '0' && s[l] <= '9')
+{
+n = n * 10;
+if (n == 2147483640)
+{
+n = n - 1;
+}
+n = n + (s[l] - '0');
+l++;
+}
+break;
+}
+}
+if (ns % 2 != 0)
+{
+n = -n;
+if (n == -2147483647)
+{
+return (n - 1);
+}
+}
+if (n == 2147483646)
+{
+return (n + 1);
+}
+return (n);
 }
