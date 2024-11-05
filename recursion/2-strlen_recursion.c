644527c57313b8afeadfc9ad77a308641a928eb3
JorreJ <jade.jorre@gmail.com>
2024-11-05 13:37:39 +0000

    create strlen_recursion.c programm

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return the lenght of a string.
+ * @s: string to claculate the lenght of
+ * 
+ * Return: lenght of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+{
+return (1 + _strlen_recursion(s + 1));
+}
+else
+{
+return (0);
+}
+}
