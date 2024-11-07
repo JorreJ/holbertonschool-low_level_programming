b1a32ed1ad0a3fecebbdabfc5656946cb93dca01
JorreJ <jade.jorre@gmail.com>
2024-11-07 14:53:10 +0000

    save temporary work

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
+ */
+
+int is_palindrome(char *s)
+{
+  
+}
+
+int _test_pal(int c1, int c2)
+{
+  if (c1 <= c2)
+    {
+      return (1);
+    }
+  
+}
