500903e9ff01649e5306a6e8a34f0337dd8ccff0
JorreJ <jade.jorre@gmail.com>
2024-11-12 19:48:53 +0100

    modify str_concat.c programm

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,13 +14,11 @@ char *str_concat(char *s1, char *s2)
 {
 int l;
 char *s;
-s = malloc((_strlen(s1) + _strlen(s2)) * sizeof(char) + 1);
-if (s == NULL)
-{
-return (NULL);
-}
 if (s1 != NULL && s2 != NULL)
 {
+s = malloc((_strlen(s1) + _strlen(s2) * sizeof(char) + 1));
+if (s == NULL)
+return (NULL);
 for (l = 0; l < _strlen(s1); l++)
 {
 *(s + l) = *(s1 + l);
@@ -32,6 +30,9 @@ for (l = 0; l < _strlen(s2); l++)
 }
 else if (s1 != NULL)
 {
+s = malloc(_strlen(s1) * sizeof(char) + 1);
+if (s == NULL)
+return (NULL);
 for (l = 0; l < _strlen(s1); l++)
 {
 *(s + l) = *(s1 + l);
@@ -39,6 +40,9 @@ for (l = 0; l < _strlen(s1); l++)
 }
 else if (s2 != NULL)
 {
+s = malloc(_strlen(s2) * sizeof(char) + 1);
+if (s == NULL)
+return (NULL);
 for (l = 0; l < _strlen(s2); l++)
 {
 *(s + l) = *(s2 + l);
