80bbe0b4653a20a921eff3ae1231f92e60c51ab8
JorreJ <jade.jorre@gmail.com>
2024-11-12 13:26:32 +0100

    create strdup.c programm

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include "../pointers_arrays_strings/2-strlen.c"
+
+/**
+ * _strdup - return a pointer to a copy of string str
+ * @str: string to copy
+ *
+ * Return: pointer to copy of str
+ */
+
+char *_strdup(char *str)
+{
+char *sc;
+int l;
+if (str == NULL)
+{
+return (NULL);
+}
+sc = malloc(_strlen(str) * sizeof(char));
+if (sc == NULL)
+{
+return (NULL);
+}
+for (l = 0; l < _strlen(str); l++)
+{
+*(sc + l) = *(str + l);
+}
+*(sc + l) = '\0';
+return (sc);
+}
