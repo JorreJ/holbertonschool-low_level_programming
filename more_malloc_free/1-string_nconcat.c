1f75ccf8d8b9217b9aff790c8adc91f1f9f63ae2
JorreJ <jade.jorre@gmail.com>
2024-11-13 22:47:58 +0100

    corrected betty

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,36 +13,36 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *s3;
-    int l1, l2;
-    if (s1 != NULL && s2 != NULL)
-    {
-        s3 = malloc(sizeof(char) * _strlen(s1) + n + 1);
-    }
-    else if (s1 != NULL && s2 == NULL)
-    {
-        s3 = malloc(sizeof(char) * _strlen(s1) + 1);
-    }
-    else if (s1 == NULL && s2 != NULL)
-    {
-        s3 = malloc(n + 1);
-    }
-    else
-    {
-        s3 = malloc(1);
-    }
-    if (s3 == NULL)
-    {
-        return (NULL);
-    }
-    for (l1 = 0; l1 < _strlen(s1) && s1 != NULL; l1++)
-    {
-        *(s3 + l1) = *(s1 + l1);
-    }
-    for (l2 = 0; (unsigned int)l2 < n && s2 != NULL; l2++)
-    {
-        *(s3 + l1 + l2) = *(s2 + l2);
-    }
-    *(s3 +l1 +l2 + 1) = '\0';
-    return (s3);
+char *s3;
+int l1, l2;
+if (s1 != NULL && s2 != NULL)
+{
+s3 = malloc(sizeof(char) * _strlen(s1) + n + 1);
+}
+else if (s1 != NULL && s2 == NULL)
+{
+s3 = malloc(sizeof(char) * _strlen(s1) + 1);
+}
+else if (s1 == NULL && s2 != NULL)
+{
+s3 = malloc(n + 1);
+}
+else
+{
+s3 = malloc(1);
+}
+if (s3 == NULL)
+{
+return (NULL);
+}
+for (l1 = 0; l1 < _strlen(s1) && s1 != NULL; l1++)
+{
+*(s3 + l1) = *(s1 + l1);
+}
+for (l2 = 0; (unsigned int)l2 < n && s2 != NULL; l2++)
+{
+*(s3 + l1 + l2) = *(s2 + l2);
+}
+*(s3 + l1 + l2 + 1) = '\0';
+return (s3);
 }
