a6274e242e73dcd103ea70828d6fba0c8871b5bb
JorreJ <jade.jorre@gmail.com>
2024-11-14 14:32:46 +0100

    include guards

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,19 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - initialize structure dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: its a dog :)
+ */
+
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
+#endif
