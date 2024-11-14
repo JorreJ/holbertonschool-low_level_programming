844a0d62fb715b3fc517db6c8ac4894ed0236e26
JorreJ <jade.jorre@gmail.com>
2024-11-14 15:41:31 +0100

    modify print_dog.c programm

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,11 +15,11 @@ return;
 }
 if ((*d).name == NULL)
 {
-(*d).name = "nil";
+(*d).name = "(nil)";
 }
 if ((*d).owner == NULL)
 {
-(*d).owner = "nil";
+(*d).owner = "(nil)";
 }
 printf("Name: %s\n", (*d).name);
 printf("Age: %f\n", (*d).age);
