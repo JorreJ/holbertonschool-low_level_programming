177502534805c73225eaad4eee03844e1564554b
JorreJ <jade.jorre@gmail.com>
2024-11-15 14:07:20 +0100

    create free_dog.c programm

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: dog to free
+ */
+
+void free_dog(dog_t *d)
+{
+free((*d).name);
+free((*d).owner);
+free(d);
+}
