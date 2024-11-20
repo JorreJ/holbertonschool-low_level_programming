7fb608f8874ca8ca9299921635b2996ec846837e
JorreJ <jade.jorre@gmail.com>
2024-11-20 13:01:44 +0100

    modify calc programm

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -13,9 +13,9 @@ int (*get_op_func(char *s))(int, int)
 {
     op_t ops[] = {{"+", op_add},{"-", op_sub},{"*", op_mul},{"/", op_div},{"%", op_mod},{NULL, NULL}};
     int i = 0;
-    while (i < 5)
+    while (ops[i].op)
     {
-        if (*s == ops[i].op[0])
+        if (strcmp(s, ops[i].op) == 0)
         {
             return (ops[i].f);
         }
