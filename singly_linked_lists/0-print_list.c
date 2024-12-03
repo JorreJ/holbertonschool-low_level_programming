7f796e639c8c526c8189eabce891740c190a7719
JorreJ <jade.jorre@gmail.com>
2024-12-03 13:55:11 +0100

    fix betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,19 +8,20 @@
 
 size_t print_list(const list_t *h)
 {
-size_t s = 0;
-while (h)
-{
-if ((*h).str == NULL)
-{
-printf("[0] (nil)\n");
-}
-else
-{
-printf("[%u] %s\n", (*h).len, (*h).str);
-}
-s++;
-h = (*h).next;
-}
-return (s);
+	size_t s = 0;
+
+	while (h)
+	{
+		if ((*h).str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", (*h).len, (*h).str);
+		}
+		s++;
+		h = (*h).next;
+	}
+	return (s);
 }
