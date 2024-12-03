e0dfd365e262b9af4bba2b2ba3fe5b4502e98be5
JorreJ <jade.jorre@gmail.com>
2024-12-03 13:44:44 +0100

    create print_list.c programm

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the list_t list
+ * Return: number of elements print
+ */
+
+size_t print_list(const list_t *h)
+{
+size_t s = 0;
+while (h)
+{
+if ((*h).str == NULL)
+{
+printf("[0] (nil)\n");
+}
+else
+{
+printf("[%u] %s\n", (*h).len, (*h).str);
+}
+s++;
+h = (*h).next;
+}
+return (s);
+}
